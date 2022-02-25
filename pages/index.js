b2c6047e602d6e02b391a9434aa281a1f6016468
Avinash-Ashokar <mail_avinashashokar@pm.me>
2022-02-25 20:14:36 +0530

    Created Meetup Details Page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,55 @@
-export default function Index() {
-  return (
-    <div>
-      <h1> This is the root page !!!</h1>
-    </div>
-  );
+import MeetupList from "../components/meetups/MeetupList";
+
+export default function HomePage() {
+  const DUMMY_MEETUPS = [
+    {
+      id: "1",
+      title: "First",
+      image:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2017/12/12122453873_1f3e1a30f0_k.jpg",
+      address: "Area 51",
+      description: "The first Meetup",
+    },
+    {
+      id: "2",
+      title: "Second",
+      image:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2017/12/12122453873_1f3e1a30f0_k.jpg",
+      address: "Area 51",
+      description: "The first Meetup",
+    },
+    {
+      id: "3",
+      title: "First",
+      image:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2017/12/12122453873_1f3e1a30f0_k.jpg",
+      address: "Area 51",
+      description: "The first Meetup",
+    },
+    {
+      id: "4",
+      title: "First",
+      image:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2017/12/12122453873_1f3e1a30f0_k.jpg",
+      address: "Area 51",
+      description: "The first Meetup",
+    },
+    {
+      id: "5",
+      title: "First",
+      image:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2017/12/12122453873_1f3e1a30f0_k.jpg",
+      address: "Area 51",
+      description: "The first Meetup",
+    },
+    {
+      id: "6",
+      title: "First",
+      image:
+        "https://cdn.theculturetrip.com/wp-content/uploads/2017/12/12122453873_1f3e1a30f0_k.jpg",
+      address: "Area 51",
+      description: "The first Meetup",
+    },
+  ];
+  return <MeetupList meetups={DUMMY_MEETUPS} />;
 }
---
+++ pages/new-meetup/index.js
@@ -0,0 +1,9 @@
+import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+
+export default function NewMeetupPage() {
+  const onAddMeetupHandler = (enteredMeetupData) => {
+    console.log(enteredMeetupData);
+  };
+
+  return <NewMeetupForm onAddMeetup={onAddMeetupHandler} />;
+}
